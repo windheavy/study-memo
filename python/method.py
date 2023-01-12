ec973a1b4b6355aa92f6a9c8872ecd4cd04120d0
wind heavy <46210070+windheavy@users.noreply.github.com>
2023-01-12 17:49:44 +0900

    sss

---
+++ python/method.py
@@ -0,0 +1,8 @@
+text = "lower letters" # 変数 text に 文字列 "lower letters"を代入
+uppered_text = text.upper() # upper() メソッドで、大文字を作成
+print(uppered_text)
+
+text = "The shells she sells are sea-shells, I'm sure."
+res_1 = text.find("sea")
+print(res_1)
+
